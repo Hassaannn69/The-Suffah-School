bb343d60b318582f2dc0c2c08b36828f98e245c5
Hassaannn69 <hassaan@thesuffahschool.com>
2025-12-05 22:29:26 +0500

    Configure Supabase to use sessionStorage (clears on browser close)

--- assets/js/supabase-init.js
+++ assets/js/supabase-init.js
@@ -10,6 +10,7 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     auth: {
         persistSession: true,
+        storage: window.sessionStorage, // Use sessionStorage to clear on browser close
         autoRefreshToken: true,
         detectSessionInUrl: true
     }
